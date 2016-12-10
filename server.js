64ecc183f6cbcec2b2bece898ac9a79f38d964ae
Norbert Szucs <szucs.norbert@outlook.com>
2016-12-10 21:02:15 +0100

    Add dynamic PORT

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(9000);
+const PORT = process.env.PORT || 9000;
+app.listen(PORT);
